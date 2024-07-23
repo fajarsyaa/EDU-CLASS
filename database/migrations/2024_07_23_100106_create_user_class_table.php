bf4fa54ab6c82212c9626ba4f0d6cef0ee41eb47
rafidaaziz23 <dev.rafidaaziz@gmail.com>
2024-07-23 17:08:11 +0700

    init migration

--- database/migrations/2024_07_23_100106_create_user_class_table.php
+++ database/migrations/2024_07_23_100106_create_user_class_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_class', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('fullname');
-            $table->string('role');
+            $table->foreignId('class_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_class');
     }
 };
--- database/migrations/2024_07_23_100107_create_comments_table.php
+++ database/migrations/2024_07_23_100107_create_comments_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('fullname');
-            $table->string('role');
+            $table->foreignId('module_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('message');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
